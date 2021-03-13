571a5cc956de267922986b833490749c15b92546
Liam Bindle <lrbindle@gmail.com>
2021-03-13 01:53:13 -0600

    added basic tools

--- gridspec/cli.py
+++ gridspec/cli.py
@@ -108,4 +108,122 @@ def show(filepath):
     if not is_mosaic and not is_tile:
         raise click.BadParameter(f"{filepath} is not a gridspec file")
 
+@utils.command()
+@click.argument('datafile',
+                nargs=-1, required=True,
+                type=click.Path(exists=True, file_okay=True, dir_okay=False, writable=False, readable=True))
+@click.option('-m', '--mosaic',
+              type=click.Path(exists=True, file_okay=True, dir_okay=False, writable=False, readable=True),
+              required=True,
+              help="Path to gridspec mosaic")
+@click.option('-d', '--dim',
+              type=click.STRING, metavar="NAME", required=True,
+              help="The name of tile dimension in the data files")
+@click.option("-o", '--output-dir',
+              type=click.Path(exists=True, file_okay=False, dir_okay=True, writable=True),
+              metavar="PATH",
+              default="./",
+              help="The directory that the mosaic file is saved to")
+def split_datafile(datafile, mosaic, dim, output_dir):
+    """
+    Split a (stacked) data file into separate data files for each tile.
+
+    DATAFILE... are the data files that are split.
+    """
+    from gridspec.misc.datafile_ops import split_datafile
+
+    click.echo(f'Splitting {len(datafile)} datafiles along dimension "{dim}"')
+    new_files = []
+    for f in datafile:
+        split_files = split_datafile(f, dim, mosaic, directory=output_dir)
+        for file in split_files:
+            click.echo(f"  + {file}")
+        new_files.extend(split_files)
+    click.echo(f'\nCreated {len(new_files)} files.')
+
+@utils.command()
+@click.argument('file_prefix', nargs=-1, required=True, type=click.STRING)
+@click.option('-m', '--mosaic',
+              type=click.Path(exists=True, file_okay=True, dir_okay=False, writable=False, readable=True),
+              required=True,
+              help="Path to gridspec mosaic")
+@click.option("-o", '--output-dir',
+              type=click.Path(exists=True, file_okay=False, dir_okay=True, writable=True),
+              metavar="PATH",
+              default="./",
+              help="The directory that the mosaic file is saved to")
+def new_datafiles(file_prefix, mosaic, output_dir):
+    """
+    Create new empty data files. This is useful for the --dstdatafile argument in ESMF_Regrid.
+
+    FILE_PREFIX... are the name prefixes for the empty data files that are created.
+    """
+    from gridspec.misc.datafile_ops import touch_datafiles
+
+    click.echo(f'Creating empty data files.')
+    new_files = []
+    for fprefix in file_prefix:
+        files = touch_datafiles(mosaic, fprefix, directory=output_dir)
+        new_files.extend(files)
+        for f in files:
+            click.echo(f"  + {f}")
+    click.echo(f'\nCreated {len(new_files)} files.')
+
+
+@utils.command()
+@click.argument('file_prefix', nargs=-1, required=True, type=click.STRING)
+@click.option('-m', '--mosaic',
+              type=click.Path(exists=True, file_okay=True, dir_okay=False, writable=False, readable=True),
+              required=True,
+              help="Path to gridspec mosaic")
+@click.option('-d', '--dim',
+              type=click.STRING, metavar="NAME", required=True,
+              help="The name of tile dimension in the data files")
+@click.option('-s', '--spec',
+              type=click.File(), metavar="JSONSPEC", required=True,
+              help="The joining spec (JSON) file path")
+@click.option("-o", '--output-dir',
+              type=click.Path(exists=True, file_okay=False, dir_okay=True, writable=True),
+              metavar="PATH",
+              default="./",
+              help="The directory that the mosaic file is saved to")
+def join_datafiles(file_prefix, mosaic, dim, spec, output_dir):
+    """
+    Create new empty data files. This is useful for the --dstdatafile argument in ESMF_Regrid.
+
+    FILE_PREFIX... are the name prefixes for the empty data files that are created.
+    """
+    import json
+    from gridspec.misc.datafile_ops import join_datafiles
+    from pathlib import Path
+    click.echo('Loading join specification')
+    join_spec = json.loads(spec.read())
+
+    rename_pairs = join_spec.get('rename_dict', {})
+    if len(rename_pairs) > 0:
+        msg = '  Renaming:' + ", ".join(f"{o} to {n}" for o, n in rename_pairs.items())
+        click.echo(msg)
+    coord_attrs = join_spec.get('coord_attrs_dict', {})
+    if len(coord_attrs) > 0:
+        for coord, attr in coord_attrs.items():
+            click.echo(f'  Assigning attributes:')
+            for k, v in attr.items():
+                click.echo(f'    {coord}:{k}="{v}"')
+    transpose = join_spec.get('transpose', [])
+    if len(transpose) > 0:
+        msg = '  Transpose: (' + ", ".join([t for t in transpose]) + ")"
+        click.echo(msg)
+    else:
+        transpose=None
+
+    click.echo(f'\nJoining data files.')
+    new_files = []
+    for fprefix in file_prefix:
+        file = join_datafiles(fprefix, mosaic, tile_dim=dim, directory=output_dir, rename_dict=rename_pairs, coord_attrs_dict=coord_attrs, transpose=transpose)
+        new_files.append(file)
+        filesize = round(Path(file).stat().st_size / 1024**2)
+        click.echo(f"  + {file} ({filesize:,} MB)")
+    click.echo(f'\nCreated {len(new_files)} files.')
+
+
 
